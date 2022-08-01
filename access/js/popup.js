3b97777b256bebd829f38d5cc39074fac79ce86e
wasabi <shulgach2013@gmail.com>
2022-08-01 17:44:48 +0300

    added popup

--- access/js/popup.js
+++ access/js/popup.js
@@ -1,1 +1,12 @@
-document.querySelector(body).add("overlay")
+function popup(){
+    document.getElementById("overlay").style.visibility = 'visible';
+    document.getElementById("popup").style.visibility = 'visible';
+    document.getElementById("popup").style.opacity = '100';
+    document.getElementById("popup").style.zIndex = '100';
+}
+function closePopup(){
+    document.getElementById("overlay").style.visibility = 'hidden';
+    document.getElementById("popup").style.visibility = 'hidden';
+    document.getElementById("popup").style.opacity = '0';
+    document.getElementById("popup").style.zIndex = '0';
+}
